a61c6d0a98a9e28416c867c4112a4de65f303dd8
ykyki <bsyki@pm.me>
2025-06-28 13:46:02 +0900

    nvim: disable auto session

--- nvim/lua/config/lazy-nvim.lua
+++ nvim/lua/config/lazy-nvim.lua
@@ -167,18 +167,18 @@ local plugins = {
   },
 
   -- Restore Session
-  {
-    -- https://github.com/rmagatti/auto-session
-    'rmagatti/auto-session',
-    lazy = false,
-
-    ---enables autocomplete for opts
-    ---@module "auto-session"
-    ---@type AutoSession.Config
-    opts = {
-      suppressed_dirs = { '~/', '~/Downloads', '/' },
-    }
-  },
+  -- {
+  --   -- https://github.com/rmagatti/auto-session
+  --   'rmagatti/auto-session',
+  --   lazy = false,
+  --
+  --   ---enables autocomplete for opts
+  --   ---@module "auto-session"
+  --   ---@type AutoSession.Config
+  --   opts = {
+  --     suppressed_dirs = { '~/', '~/Downloads', '/' },
+  --   }
+  -- },
 
   {
     -- https://github.com/lewis6991/gitsigns.nvim
