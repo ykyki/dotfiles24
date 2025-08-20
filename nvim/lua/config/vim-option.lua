728f90160b9e6859c340545f45cb3d6d22550200
ykyki <bsyki@pm.me>
2025-08-20 23:25:59 +0900

    nvim: set autoread and format

--- nvim/lua/config/vim-option.lua
+++ nvim/lua/config/vim-option.lua
@@ -1,34 +1,35 @@
 local options = {
-  swapfile       = false, -- スワップファイルの生成
-  backup         = false, -- バックアップファイルの生成
-  hidden         = true,  -- バッファが編集中でもその他のファイルを開けるようにするか否か
-  confirm        = true,  -- 保存されていないファイルがあるときは終了前に保存確認するか否か
-  autoread       = true,  -- 編集中のファイルが変更されたら自動で読み直すか否か
+  swapfile       = false,   -- スワップファイルの生成
+  backup         = false,   -- バックアップファイルの生成
+  hidden         = true,    -- バッファが編集中でもその他のファイルを開けるようにするか否か
+  confirm        = true,    -- 保存されていないファイルがあるときは終了前に保存確認するか否か
+  autoread       = true,    -- 編集中のファイルが変更されたら自動で読み直すか否か
+  updatetime     = 250,     -- CursorHoldイベントの間隔とswapファイル書き込み間隔（ms）
 
-  number         = true,  -- 行番号の表示
-  relativenumber = false, -- 相対行番号にするか否か
-  cursorline     = false,  -- カーソル行のハイライト
+  number         = true,    -- 行番号の表示
+  relativenumber = false,   -- 相対行番号にするか否か
+  cursorline     = false,   -- カーソル行のハイライト
 
-  tabstop        = 2,     -- Number of spaces that a <Tab> in the file counts for
-  shiftwidth     = 2,     -- Number of spaces to use for each step of (auto)indent
-  expandtab      = true,  -- Use the appropriate number of spaces to insert a <Tab>
+  tabstop        = 2,       -- Number of spaces that a <Tab> in the file counts for
+  shiftwidth     = 2,       -- Number of spaces to use for each step of (auto)indent
+  expandtab      = true,    -- Use the appropriate number of spaces to insert a <Tab>
 
-  wrap           = false, -- 行の折り返し
+  wrap           = false,   -- 行の折り返し
 
-  cmdheight      = 1,     -- Number of screen lines to use for the command-line
+  cmdheight      = 1,       -- Number of screen lines to use for the command-line
 
-  ignorecase     = true,  -- 検索で ignorecase を設定
-  smartcase      = true,  -- 検索で smartcase を設定
-  incsearch      = true,  -- インクリメンタル検索
-  hlsearch       = true,  -- 検索時のハイライト
+  ignorecase     = true,    -- 検索で ignorecase を設定
+  smartcase      = true,    -- 検索で smartcase を設定
+  incsearch      = true,    -- インクリメンタル検索
+  hlsearch       = true,    -- 検索時のハイライト
 
-  showmatch      = true,  -- 対応する括弧をハイライト
+  showmatch      = true,    -- 対応する括弧をハイライト
 
-  termguicolors  = true, -- 24-bit RGBを利用する
+  termguicolors  = true,    -- 24-bit RGBを利用する
 
-  nrformats      = "octal", -- 
+  nrformats      = "octal", --
 
-  mouse          = "a",  -- mouse support for all modes
+  mouse          = "a",     -- mouse support for all modes
 }
 
 for k, v in pairs(options) do
@@ -81,3 +82,14 @@ keymap("v", ">", ">gv", {})
 
 keymap("n", "Y", "y$", {})
 
+-- autoread関連の自動コマンド
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  command = "if mode() != 'c' | checktime | endif"
+})
+
+-- ファイル変更通知
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+  pattern = "*",
+  command = "echohl WarningMsg | echo \"File changed on disk. Buffer reloaded.\" | echohl None"
+})
